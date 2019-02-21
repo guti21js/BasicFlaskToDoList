afc9739c5700aab795ec7d578094a0fda5480c8f
shoomi <gutierrezjdr@gmail.com>
2019-02-20 22:18:20 -0500

    added file requirements for heroku

--- myproject/hello.py
+++ myproject/hello.py
@@ -12,6 +12,10 @@ app = Flask(__name__)
 with open('data.json') as file:
 	data = json.load(file)
 	
+if __name__ == "__main__":
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host="0.0.0.0", port=port, threaded=True)
+
 
 @app.route('/', methods=['GET','POST'])
 def register():
